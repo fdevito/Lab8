aa3331fd7f6f8a8fbe4fd9dc78dbd6b97f6df407
fdevito <fdevito@ufl.edu>
2020-04-04 22:08:25 +0000

    First commit for Lab8a

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo192.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Francesca DeVito
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/fdevito/Lab8.git"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link to github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
